616b687538c1cb2729439b3a8b01ae13768bf948
Marta Basto <87577613+mpbasto@users.noreply.github.com>
2022-05-16 14:43:06 +0100

    Adding description to functions

--- gameManager.py
+++ gameManager.py
@@ -19,16 +19,25 @@ class GameManager:
         self.game_loop(self.player, self.cars, self.scores)
 
     def screen_setup(self):
+        """
+        Sets up screen for the game.
+        """
         self.screen.clear()
         self.screen.title('Turtle Crossing')
         self.screen.tracer(0)
         self.screen.listen()
 
     def controls(self, player):
+        """
+        Function that arranges the key bindings.
+        """
         self.screen.onkeypress(player.move, 'Up')
         self.screen.onkey(self.new_game, 'space')
 
     def new_game(self):
+        """
+        Function that displays a message to confirm new game request and sets up new game.
+        """
         response = messagebox.askquestion('New Game',
                                           'Would you like to start a new game?')
         if response == 'yes':
@@ -45,6 +54,9 @@ class GameManager:
 
 
     def game_over(self):
+        """
+        Displays a 'game over' message and asks user if they want to start a new game. If 'yes', sets up a new game, otherwise if quits game.
+        """
         response = messagebox.askquestion('GAME OVER', f'GAME OVER!\n\n Congrats, you manage to reach level {self.scores.level}!\n\n Would you like to start again?')
 
         if response == 'yes':
@@ -61,7 +73,10 @@ class GameManager:
 
 
     def game_loop(self, player, cars, score):
-
+        """
+        Function that organises the game itself: collisions, successful crossings and levels.
+        """
+        
         game_is_on = True
         while game_is_on:
             time.sleep(0.1)
